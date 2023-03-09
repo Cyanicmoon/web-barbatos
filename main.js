95627607fb7281c9c3607a07352c3d8b0b5604d0
Lati <95179689+Cyanicmoon@users.noreply.github.com>
2023-03-09 22:08:14 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-function goBlog(){
-    window.location.href = 'https://blog.naver.com/aiden-1234';
+function clicked_venti(){
+    window.location.href = "./character-info/venti.html";
+}
+function clicked_hutao(){
+    window.location.href = "./character-info/hutao.html";
 }
