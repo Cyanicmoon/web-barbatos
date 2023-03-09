0182e80c6a9abbb8cab1da72d2a294513b9751a1
Lati <95179689+Cyanicmoon@users.noreply.github.com>
2023-03-09 22:27:26 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-
+var URLString = top.location.href + "/";
+tURLString = URLString.split("/");
+iii = tURLString.length > 2 ? (tURLString[3].length > 0 ? false:true) : false
+if(!iii){
+    document.cookie = "UrlString=" + top.window.location.href + "; path=/;";
+    top.window.location.href = "http://"+top.window.location.host;
+}
 
 function clicked_venti(){
     window.location.href = "./character-info/venti.html";
