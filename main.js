894f42f8570038363eae22265012d3c9895d081d
Lati <95179689+Cyanicmoon@users.noreply.github.com>
2023-03-09 22:20:35 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+/*function goBlog(){
+    window.location.href = 'https://blog.naver.com/aiden-1234';
+}
+
+function clicked_submit(){
+    var textarea = document.getElementById('submit');
+    console.log(typeof(textarea.value));
+}
+*/
+
+function clicked_venti(){
+    window.location.href = "./character-info/venti.html";
+}
+function clicked_hutao(){
+    window.location.href = "./character-info/hutao.html";
+}
